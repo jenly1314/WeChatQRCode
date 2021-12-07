051a13f7fb86e284c97fc05dd04a8ae2e77afcd5
Jenly <jenly1314@gmail.com>
2021-12-07 10:33:24 +0800

    CI

--- src/test/java/com/king/wechat/qrcode/Demo.java
+++ src/test/java/com/king/wechat/qrcode/Demo.java
@@ -1,10 +1,27 @@
 package com.king.wechat.qrcode;
 
+import org.junit.Test;
+
 import java.util.List;
 
 public class Demo {
 
     public static void main(String[] args) {
+        try {
+            //初始化 OpenCV
+            OpenCV.init();
+            //初始化 WeChatQRCode
+            WeChatQRCodeDetector.init();
+            //检测并识别二维码 （同一张图片可能有多个二维码）
+            List<String> results = WeChatQRCodeDetector.detectAndDecode("image/WeChatQRCode.png");
+            System.out.println("results:" + results);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void test(){
         try {
             //初始化 OpenCV
             OpenCV.init();
